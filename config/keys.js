e1466205d5a905fb912d3706e623e51ce918a5ac
Shangni <shangnihu@gmail.com>
2019-04-14 22:53:22 -0400

    Connection works now

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://localhost:27017/mern-shopping"
+    "mongodb://shangnihu:shangni1234@mern-shopping-shard-00-00-v4qmy.mongodb.net:27017,mern-shopping-shard-00-01-v4qmy.mongodb.net:27017,mern-shopping-shard-00-02-v4qmy.mongodb.net:27017/test?ssl=true&replicaSet=mern-shopping-shard-0&authSource=admin&retryWrites=true"
+    // "mongodb://localhost:27017/mern-shopping"
 };
